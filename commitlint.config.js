660b54769722c60dd888fe6e48a3bb872ba90b91
Aditya <adityasinghboss1234@gmail.com>
2023-07-07 22:30:38 +0530

    feat(*): husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   exports: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "chore"]],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-max-length": [2, "always", 72],
+  },
 };
